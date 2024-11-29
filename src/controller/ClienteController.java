dd1151971f235a637e5b275da635d403a30a19b3
Larré21 <gugularre@gmail.com>
2024-11-29 02:20:35 -0300

    gg

--- src/controller/ClienteController.java
+++ src/controller/ClienteController.java
@@ -1,5 +1,35 @@
 package controller;
 
+import dao.ClienteDAO;
+import model.Cliente;
+
 public class ClienteController {
-    
+    private ClienteDAO clienteDAO; // Instância do DAO para acessar dados do banco
+
+    //Construtor que inicializa o ClienteDao
+    public ClienteController(){
+        // Aqui pode-se passar a conexão com o banco de dados
+        this.clienteDAO = new ClienteDAO();
+    }
+
+    //Método para cadastrar um novo cliente
+    public String cadastrarCliente(String nome, String CPF, String CEP, String Cidade, String Estado, String Telefone, String email, String Senha){
+        
+        //Cria uma instância do modelo Cliente com os dados recebidos
+        Cliente cliente = new Cliente(nome, CPF, CEP, Cidade, Estado, Telefone, email, Senha);
+
+        //Chama o método do ClienteDAO para salvar o cliente no banco
+        boolean sucesso = clienteDAO.cadastrarCliente(cliente);
+
+        //Retrona uma mensagem indicando o sucesso ou falha do cadastro
+        if(sucesso){
+            return "Cliente cadastrado com sucesso!";
+        }else{
+            return "Erro ao cadastrar cliente!";
+        }
+    }
+
 }
+
+
+
--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -2,10 +2,31 @@ package dao;
 
 import model.Cliente;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteDAO {
+
+    private class Connection connection;
+
+    //Construtor que inicializa a conexão com o banco
+    public ClienteDAO(){
+        //Inicializar a conexão com o banco
+        this.connection = ConnectionFactory.getConexao();
+    }
+
+    //Método para cadastrar cliente no banco de dados
+    public boolean cadastrarCliente(Cliente cliente){
+        String sql = "INSERT INTO cliente (nome, CPF, CEP, Cidade, Estado, email, Senha) VALUES (?,?,?,?,?,?,?)";
+
+        try (PreparedStatement stmt = connection.PreparedStatement(sql)){
+            //Definir os valore para o query
+            stmt.setString(1, cliente.getNome());
+            stmt.setString(, sql);
+        }
+    }
     
 }
