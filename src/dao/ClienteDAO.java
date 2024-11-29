21ba541bd1f8356c80a87244e2230af728e7e29e
Gabriel Sousa <devgabrielk@gmail.com>
2024-11-29 02:33:57 -0300

    Testes

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -1,6 +1,7 @@
 package dao;
 
-import model.Cliente;
+import model.Cliente;´
+import util.Banco;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,24 +10,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteDAO {
-
-    private class Connection connection;
+    
+    private Connection connection;
 
     //Construtor que inicializa a conexão com o banco
-    public ClienteDAO(){
-        //Inicializar a conexão com o banco
-        this.connection = ConnectionFactory.getConexao();
+    public ClienteDAO() throws SQLException {
+        // Inicializar a conexão com o banco e propagar a exceção
+        this.connection = Banco.getConexao();
     }
 
     //Método para cadastrar cliente no banco de dados
-    public boolean cadastrarCliente(Cliente cliente){
+    public boolean cadastrarCliente(Cliente cliente) {
         String sql = "INSERT INTO cliente (nome, CPF, CEP, Cidade, Estado, email, Senha) VALUES (?,?,?,?,?,?,?)";
 
-        try (PreparedStatement stmt = connection.PreparedStatement(sql)){
-            //Definir os valore para o query
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            // Definir os valores para o query
             stmt.setString(1, cliente.getNome());
-            stmt.setString(, sql);
+            stmt.setString(2, cliente.getCpf());
+            stmt.setString(3, cliente.getCep());
+            stmt.setString(4, cliente.getCidade());
+            stmt.setString(5, cliente.getEstado());
+            stmt.setString(6, cliente.getEmail());
+            stmt.setString(7, cliente.getSenha());
+            
+            // Executar a atualização no banco de dados
+            stmt.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace(); // Imprimir o erro de SQL
+            return false;
         }
     }
-    
 }
