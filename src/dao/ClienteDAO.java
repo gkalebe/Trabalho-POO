83b946b2d5d611381a40ba4b37596071e3c20f8a
Gabriel Sousa <devgabrielk@gmail.com>
2024-11-29 00:15:10 -0300

    Dá git pull ai mano

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -1,5 +1,11 @@
 package dao;
 
+import model.Cliente;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClienteDAO {
     
 }
--- src/util/Banco.java
+++ src/util/Banco.java
@@ -1,5 +1,25 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Banco {
-    
+    private static final String URL = "";
+    private static final String USUARIO = "";
+    private static final String SENHA = "";
+
+        public static Connection getConexao() throws SQLException{
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        }
+
+            public static void fecharConexao(Connection conexao){
+                if (conexao != null){
+                    try{
+                        conexao.close();
+                        }catch (SQLException e){
+                            e.printStackTrace();
+                        }
+                }
+            }
 }
